27147547d723f57f52aafab9692ba2c3989f79d7
TilLindenan <148491365+TilLindenan@users.noreply.github.com>
2024-01-21 12:56:45 +0300

    Add files via upload

---
+++ ja.js
@@ -0,0 +1,6 @@
+document.getElementById("vhod").addEventListener("click", function () {
+  document.getElementById("box_modal").classList.add("open");
+});
+document.getElementById("clouse").addEventListener("click", function () {
+  document.getElementById("box_modal").classList.remove("open");
+});
